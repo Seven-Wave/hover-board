9b68c455824a9fc8a8b721e91896a7e012e0ca7b
Andey <58787957+Seven-Wave@users.noreply.github.com>
2021-01-06 13:58:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARE_NM = 500
+
+for (i = 0; i < SQUARE_NM; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+    })
+    
+    square.addEventListener('mouseout', () => {
+        removeColor(square)
+    })
+    
+    container.appendChild(square)
+}
+
+function setColor(el) {
+    const color = getRandomColor()
+    el.style.backgroundColor = color
+    el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(el) {
+    el.style.backgroundColor = '#1d1d1d'
+    el.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
